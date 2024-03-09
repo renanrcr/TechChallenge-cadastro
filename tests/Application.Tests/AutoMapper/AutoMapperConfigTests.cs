066bf4d449117e8f0733da338a0d48a6eae0b702
Renan de Carvalho Rinaldi <renan.crinaldi@gmail.com>
2024-03-09 11:18:51 -0300

    feat: implementando os testes do auto mapper.

--- tests/Application.Tests/AutoMapper/AutoMapperConfigTests.cs
+++ tests/Application.Tests/AutoMapper/AutoMapperConfigTests.cs
@@ -11,12 +11,14 @@ namespace Application.Tests.AutoMapper
     {
         private readonly IMapper _mapper;
         private readonly IProdutoRepository _produtoRepository;
+        private readonly IClienteRepository _clienteRepository;
 
         public AutoMapperConfigTests()
         {
             var config = new MapperConfiguration(cfg => cfg.AddProfile<AutoMapperConfig>());
             _mapper = config.CreateMapper();
             _produtoRepository = IProdutoRepositoryMock.GetMock();
+            _clienteRepository = IClienteRepositoryMock.GetMock();
         }
 
         [Fact]
@@ -31,6 +33,32 @@ namespace Application.Tests.AutoMapper
 
             // Assert
             Assert.Equal(produto.Nome, produtoDTO.Nome);
+        }
+
+        [Fact]
+        public async Task MapearClienteParaClienteDto_DeveRetornarVerdadeiro()
+        {
+            // Arrange
+            Cliente? cliente = await new Cliente().Cadastrar(_clienteRepository, "Cliente I", "cliente@mail.com");
+
+            // Act
+            ClienteDTO clienteDTO = _mapper.Map<ClienteDTO>(cliente);
+
+            // Assert
+            Assert.Equal(cliente.Nome, clienteDTO.Nome);
+        }
+
+        [Fact]
+        public async Task MapearCategoriaProdutoParaCategoriaProdutoDto_DeveRetornarVerdadeiro()
+        {
+            // Arrange
+            CategoriaProduto? categoria = await new CategoriaProduto().Cadastrar("Categoria Lanche");
+
+            // Act
+            CategoriaProdutoDTO categoriaProdutoDTO = _mapper.Map<CategoriaProdutoDTO>(categoria);
+
+            // Assert
+            Assert.Equal(categoria.Descricao, categoriaProdutoDTO.Descricao);
         }
     }
 }
